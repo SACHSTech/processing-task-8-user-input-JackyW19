dc87e5a212c21a41a7a2bea387325ce21587aa21
JackyW29 <75390967+JackyW19@users.noreply.github.com>
2022-05-10 17:42:39 +0000

    done

--- Sketch.java
+++ Sketch.java
@@ -2,7 +2,14 @@ import processing.core.PApplet;
 
 public class Sketch extends PApplet {
 	
-	
+	boolean upPressed = false;
+  boolean downPressed = false;
+  boolean leftPressed = false;
+  boolean rightPressed = false;
+
+  float circleX = 150;
+  float circleY = 150;
+  
   /**
    * Called once at the beginning of execution, put your size all in this method
    */
@@ -11,26 +18,75 @@ public class Sketch extends PApplet {
     size(400, 400);
   }
 
+    
   /** 
    * Called once at the beginning of execution.  Add initial set up
    * values here i.e background, stroke, fill etc.
    */
   public void setup() {
-    background(210, 255, 173);
-  }
-
+    
+    background(255, 255, 255);
+    }
   /**
    * Called repeatedly, anything drawn to the screen goes here
    */
   public void draw() {
-	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
+    
+    // set background colour to white
+   background(255, 255, 255); 
+    
+    
+    // background colour changes accordingly to the first letter of a colour pressed
+    if (keyPressed) {
 
-    stroke(255);
-    line(50, 125, 70, 50);  
-  }
+      if (key == 'r') {
+        background(255, 0, 0);
+        } 
+      else if (key == 'o') {
+        background(255, 128, 0);
+        } 
+      else if (key == 'y') {
+        background(246, 255, 0);
+        } 
+      else if (key == 'g') {
+        background(0, 255, 0);
+        } 
+      else if (key == 'b') {
+        background(0, 0, 255);
+        }
+      else if (key == 'p') {
+        background(170, 0, 255);
+        } 
+      } 
+      else {
+        keyReleased();
+        background(255, 255, 255);
+        }
+    
+    // blue circle moves accordingly to what arrow directions user presses
+  if (keyPressed) {
+    if (keyCode == UP) {
+      circleY--;
+    } 
+    else if (keyCode == DOWN) {
+      circleY++;
+    } 
+    else if(keyCode == LEFT){
+      circleX--;
+    }
+    else if(keyCode == RIGHT){
+     circleX++; 
+    }
+    }
+    ellipse(circleX, circleY, 50, 50);
+      fill(0, 0, 255);
+    
+
+  // draws a  rectangle wherever the mouse cursor is
+    rect(mouseX, mouseY, 50, 50);
+    fill(255,255,0);
+
   
-  // define other methods down here.
+  
+  }
 }
